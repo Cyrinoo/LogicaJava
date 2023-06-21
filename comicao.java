98274e73e20000444ecb75d8598561f5fc99afb5
Pedro Cyrino <pedroxcani@gmail.com>
2023-06-20 23:11:24 -0300

    comicao.java

--- comicao.java
+++ comicao.java
@@ -1,6 +1,6 @@
 /* Um funcionário recebe um salário fixo mais 4% de comissão sobre as vendas. 
 Faça ̧a um programa que receba o salário fixo do funcionário e o 
-valor de suas vendas,  calcule e mostre a comissão e seu salário final
+valor de suas vendas,  calcule e mostre a comissão e seu salário final*/
 
 package com.mycompany.lista.de.exercicio.java;
 import java.util.Scanner;
@@ -15,4 +15,4 @@ public class ListaDeExercicioJava {
         double salFinal = (salfixo * vendas);
         System.out.println("A comissão final do salario é de: "+salFinal); 
     }
-} */
+} 
