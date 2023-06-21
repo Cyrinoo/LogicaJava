1a121794ea6a2d5670853f3ef59b6ac6eea09efd
Cyrinoo <131258025+Cyrinoo@users.noreply.github.com>
2023-06-20 22:35:05 -0300

    Add files via upload

---
+++ area triangulo.java
@@ -0,0 +1,19 @@
+/* Faça um programa que pede para o usuário digitar a altura e a base de um 
+triangulo, em seguida calcule e escreva a sua área.
+
+package com.mycompany.lista.de.exercicio.java;
+import java.util.Scanner;
+
+public class ListaDeExercicioJava {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+       System.out.println("Digite a Base do Triangulo: ");
+       double base = entrada.nextDouble();
+       System.out.println("Digite a altura do Triangulo: ");
+       double altura = entrada.nextDouble();
+       double area = (base*altura) / 2;
+       System.out.println("A Área do Triagulo é:"+area);
+    }
+}
+*/
---
+++ horario expresso.java
@@ -0,0 +1,15 @@
+/* Faça um programa que leia o 
+horário expresso em segundos e mostre-o em horas, minutos e segundos 
+package com.mycompany.lista.de.exercicio.java;
+import java.util.Scanner;
+
+public class ListaDeExercicioJava {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        System.out.println("Digite seu horario em segundos: ");
+        double segundos = entrada.nextDouble();
+        double horas = (segundos / 3600);
+        double minutos = (segundos % 3600)/60;
+System.out.println("O horario em segundos é de: "+segundos+", Pode se dizer que em minutos é de: "+minutos+" e em horas é de: " + horas);       
+    }
+} */
