b4b38410148d6b4404c28cc5ba03b7ac3f05e78e
Pedro Cyrino <pedroxcani@gmail.com>
2023-06-20 23:11:36 -0300

    horario expresso.java

--- horario expresso.java
+++ horario expresso.java
@@ -1,7 +1,7 @@
 /* Faça um programa que leia o 
 horário expresso em segundos e mostre-o em horas, minutos e segundos 
 package com.mycompany.lista.de.exercicio.java;
-import java.util.Scanner;
+import java.util.Scanner; */
 
 public class ListaDeExercicioJava {
     public static void main(String[] args) {
@@ -12,4 +12,4 @@ public class ListaDeExercicioJava {
         double minutos = (segundos % 3600)/60;
 System.out.println("O horario em segundos é de: "+segundos+", Pode se dizer que em minutos é de: "+minutos+" e em horas é de: " + horas);       
     }
-} */
+}
